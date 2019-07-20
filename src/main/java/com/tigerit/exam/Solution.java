57a88dd27b31b1d9f365e52f29155d4c642d5860
Zahid Al Tair <mdzahidh119@gmail.com>
2019-07-21 01:19:06 +0600

    Add Query struct

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -3,6 +3,7 @@ package com.tigerit.exam;
 
 import static com.tigerit.exam.IO.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -25,13 +26,13 @@ public class Solution implements Runnable {
                 Table table = new Table();
                 table.Name = readLine();
 
-                int[] vals = toInt(parseValue(readLine(), 2));
-                table.IdxName =  parseValue(readLine(), vals[0]);
+                int[] vals = toInt(parseValue(readLine(), " ", 2));
+                table.IdxName =  parseValue(readLine(), " ", vals[0]);
                 table.Records = new Tuple[vals[1]];
 
                 for(int j=0;j<vals[1];j++) {
                     Tuple tuple = new Tuple();
-                    tuple.data = toInt(parseValue(readLine(), vals[0]));
+                    tuple.data = toInt(parseValue(readLine(), " ", vals[0]));
                     table.Records[j] = tuple;
                 }
                 
@@ -40,18 +41,62 @@ public class Solution implements Runnable {
 
             db.generateIndex();
             db.print();
+
+            int numQuery = readLineAsInteger();
+
+            for(int i=0;i<numQuery;i++) {
+                Query query = new Query();
+                String[] select = parseValue(readLine(), " ", -1);
+
+                for(int j=1;j<select.length;j++) {
+                    if(select[j].equals("*")) {
+                        break;
+                    }
+
+                    select[j] = select[j].replace(",", "");
+                    String[] values = parseValue(select[j], "\\.", 2);
+
+                    query.Select[0].add(values[0]);
+                    query.Select[1].add(values[1]); 
+                }
+
+                for(int j=0;j<2;j++) {
+                    String[] values = parseValue(readLine(), " ", 3);
+                    query.Tables[j] = query.Rename[j] = values[1];
+                    if(values.length==3) {
+                        query.Rename[j] = values[2];
+                    }
+                }
+
+                String[] values = parseValue(readLine(), " ", 4);
+
+                for(int j=1;j<4;j+=2) {
+                    String[] val = parseValue(values[j], "\\.", 2);
+
+                    if(query.Rename[0].equals(val[0])) {
+                        query.On[0] = val[1];
+                    } else {
+                        query.On[1] = val[1];
+                    }
+                }
+
+                query.print();
+                readLine();
+            }
         }
     }
 
-    public String[] parseValue(String values, int limit) {
-        return values.split(" ", limit);
+    public String[] parseValue(String values, String exp, int limit) {
+        return values.split(exp, limit);
     }
 
     public int[] toInt(String[] values) {
         int[] val = new int[values.length];
+
         for(int i=0;i<values.length;i++) {
             val[i] = Integer.parseInt(values[i]);
         }
+
         return val;
     }
 
@@ -61,12 +106,14 @@ public class Solution implements Runnable {
 
         public void generateIndex() {
             TableID = new HashMap<String, Integer>();
+
             for(int i=0;i<Tables.length;i++) {
                 Tables[i].generateIndex();
                 TableID.put(Tables[i].Name, i);
             }
         }
 
+        // TODO remove this function
         public void print() {
             for(String key : TableID.keySet()) {
                 System.out.print(key + ": " + TableID.get(key) + " ");
@@ -88,6 +135,7 @@ public class Solution implements Runnable {
 
         public void generateIndex() {
             IdxID = new HashMap<String, Integer>();
+
             for(int i=0;i<IdxName.length;i++) {
                 IdxID.put(IdxName[i], i);
             }
@@ -114,7 +162,7 @@ public class Solution implements Runnable {
             for(int i=0;i<Records.length;i++) {
                 Records[i].print();
             }
-            
+
             System.out.println("--------------------");
         }
     }
@@ -129,6 +177,47 @@ public class Solution implements Runnable {
             }
 
             System.out.println();
+        }
+    }
+
+    public class Query {
+        ArrayList<String> Select[];
+        String[] Tables;
+        String[] Rename;
+        String[] On;
+
+        public Query() {
+            Select = new ArrayList[2];
+            Select[0] = new ArrayList<String>();
+            Select[1] = new ArrayList<String>();
+            Tables = new String[2];
+            Rename = new String[2];
+            On = new String[2];
+        }
+
+        // TODO remove this function
+        public void print() {
+            System.out.println("--------------------");
+            System.out.println("Select: ");
+
+            for(int i=0;i<2;i++) {
+                for(int j=0;j<Select[i].size();j++) {
+                    System.out.print(Select[i].get(j) + " ");
+                }
+
+                System.out.println();
+            }
+
+            System.out.println();
+            System.out.println("Tables: " + Tables[0] + " " +  Tables[1]);
+
+            System.out.println();
+            System.out.println("Rename: " + Rename[0] + " " +  Rename[1]);
+
+            System.out.println();
+            System.out.println("Join on: " + On[0] + " " + On[1]);
+
+            System.out.println("--------------------");
         }
     }
 }
